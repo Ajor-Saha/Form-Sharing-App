392ac64633ca0b07a8d6a9d2603a0f74cc5fc847
ajor-saha <sokarama79@gmail.com>
2024-02-23 15:11:07 +0600

    update

--- api/src/index.js
+++ api/src/index.js
@@ -1,26 +1,21 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 import mongoose from "mongoose";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
 mongoose
-   .connect(process.env.MONGODB_URI)
-   .then(() => {
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
     console.log("Mongodb is connected");
-   })
-   .catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-   })
+  });
 
-.then(() => {
-    app.listen(process.env.PORT || 4000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+});
