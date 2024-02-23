71f8b254aa3b13e53ec06e4cc4ace143a460561d
ajor-saha <sokarama79@gmail.com>
2024-02-23 15:19:32 +0600

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://form-sharing-app.vercel.app',
         secure: false,
       },
     },
